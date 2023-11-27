89fd7cc8eeb2420299b2b31e80869598ede66b8e
Darijan Sekerov <darijan2002@gmail.com>
2023-11-27 17:52:34 +0100

    fix rsc encoder

--- RSC.py
+++ RSC.py
@@ -2,13 +2,17 @@ class RSC:
     def __init__(self):
         self.mem = [0, 0]
 
-    def push(self, input):
-        ret = self.mem[1] ^ input
+    def push(self, x):
+        ret = self.mem[1] ^ x
         self.mem[1] = self.mem[0]
-        self.mem[0] = input
+        self.mem[0] = ret
 
         return ret
 
     def terminate(self):
-        return self.push(0)
+        ret = self.mem[1]
+        self.mem[1] = self.mem[0]
+        self.mem[0] = 0
+
+        return ret
 
